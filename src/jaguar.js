37e4802e1ca394f1ec0fc2f16089763822039502
Peter Cannici <turkchess123@gmail.com>
2011-09-22 12:22:13 -0600

    add :matches pseudo class

--- src/jaguar.js
+++ src/jaguar.js
@@ -557,6 +557,13 @@ Jaguar.pseudos = pseudos = {
         return getAttribute(elem, 'text').indexOf(text.value) > -1;
     },
     
+    'matches': function (elem, regex) {
+        var parts = regex.value.split('/');
+        if (parts.length == 1)
+            parts = [0, parts[0], ''];
+        return RegExp(parts[1], parts[2]).test(getAttribute(elem, 'text'));
+    },
+    
     'not': function (elem) {
         for (var selector = '', args = arguments, i = 1, l = args.length; i < l; ++i)
             selector += args[i].value;
